4d995644a5e4d7532ab9a6398844c209055897f5
Oleg Sheydvasser <oleg.sheydvasser@autodesk.com>
2022-08-31 13:00:30 -0400

    3. Building the scene

--- src/visualize/extrude.js
+++ src/visualize/extrude.js
@@ -12,6 +12,12 @@ const createBuilding = (coordinates, height) => {
   const [[x0, y0], [x1, y1], [x2, y2], [x3, y3]] = coordinates;
 
   const shape = new THREE.Shape();
+  shape.moveTo(x0, y0);
+  shape.lineTo(x1, y1);
+  shape.lineTo(x2, y2);
+  shape.lineTo(x3, y3);
+  shape.lineTo(x0, y0);
+
   const geometry = new THREE.ExtrudeGeometry(shape, {
     depth: height,
     bevelEnabled: false
