462056eee9a569a5fffc3498f1b1f50e7aa62777
Oleg Sheydvasser <oleg.sheydvasser@autodesk.com>
2022-09-02 12:58:49 -0400

    two bugs in build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p out
-emcc -std=c++14 -g -s MODULARIZE=1 -s EXPORTED_FUNCTIONS='["_move"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' -s WASM=1 -s MODULARIZE=1 -Isrc/solver src/solver/main.cpp src/solver/*.cpp -o out/solver.js
+emcc -std=c++14 -g -s MODULARIZE=1 -s EXPORTED_FUNCTIONS='["_move"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' -s WASM=1 -s MODULARIZE=1 -Isrc/solver src/solver/*.cpp -o out/solver.js
 
-sed -i '' 's:solver.wasm:out/solver.wasm:g' out/solver.js
+sed -i 's:solver.wasm:out/solver.wasm:g' out/solver.js
 echo "export { Module as default }" >> out/solver.js
